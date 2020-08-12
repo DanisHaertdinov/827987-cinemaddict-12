ae050e50c7d9536bf79c960c8faf9f562739cf10
Danis Haertdiov <danis.haertdinov@gmail.com>
2020-08-13 02:40:43 +0400

    отрисовка карточек и логика кнопки showMore

--- src/main.js
+++ src/main.js
@@ -9,16 +9,24 @@ import {createFilmDetailsTemplate} from "./view/film-details";
 import {generateFilms} from "./mock/film";
 
 const FilmsCount = {
-  DEFAULT: 10,
+  DEFAULT: 22,
   EXTRA: 2,
+  PER_STEP: 5,
 };
 
 const films = generateFilms(FilmsCount.DEFAULT);
+console.log(films)
+const topRatedFilms = films.slice().sort((a, b) => {
+  return b.rating - a.rating;
+}).slice(0, FilmsCount.EXTRA);
 
-const render = (container, template, place = `beforeend`, count = 1) => {
-  for (let i = 0; i < count; i++) {
-    container.insertAdjacentHTML(place, template);
-  }
+const mostCommentedFilms = films.slice().sort((a, b) => {
+  return b.comments.length - a.comments.length;
+}).slice(0, FilmsCount.EXTRA);
+
+
+const render = (container, template, place = `beforeend`) => {
+  container.insertAdjacentHTML(place, template);
 };
 
 const siteBodyElement = document.querySelector(`body`);
@@ -35,19 +43,46 @@ render(siteMainElement, createFilmsSectionTemplate());
 const filmsListElement = siteMainElement.querySelector(`.films-list`);
 const filmsWrapperElement = filmsListElement.querySelector(`.films-list__container`);
 
-for (const film of films) {
+films.slice(1, FilmsCount.PER_STEP + 1).forEach((film) => {
   render(filmsWrapperElement, createFilmTemplate(film));
-}
+});
 
-render(filmsListElement, createShowMoreButtonTemplate());
+if (films.length > FilmsCount.PER_STEP) {
+  let renderedFilmsCount = FilmsCount.PER_STEP;
+
+  render(filmsListElement, createShowMoreButtonTemplate());
+
+  const showMoreButtonElement = filmsListElement.querySelector(`.films-list__show-more`);
+
+  showMoreButtonElement.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+    films.slice(renderedFilmsCount, renderedFilmsCount + FilmsCount.PER_STEP).forEach((film) => {
+      render(filmsWrapperElement, createFilmTemplate(film));
+    });
+
+    renderedFilmsCount += FilmsCount.PER_STEP;
+
+    if (renderedFilmsCount >= films.length) {
+      showMoreButtonElement.remove();
+    }
+  });
+}
 
 const extraFilmsWrapperElements = siteMainElement.querySelectorAll(`.films-list--extra .films-list__container`);
 
-// for (const wrapper of extraFilmsWrapperElements) {
-//   render(wrapper, createFilmTemplate(), `beforeend`, FilmsCount.EXTRA);
-// }
+for (const wrapper of extraFilmsWrapperElements) {
+  if (wrapper === extraFilmsWrapperElements[0]) {
+    for (const film of topRatedFilms) {
+      render(wrapper, createFilmTemplate(film));
+    }
+  } else {
+    for (const film of mostCommentedFilms) {
+      render(wrapper, createFilmTemplate(film));
+    }
+  }
+}
 
 const footerStatisticsElement = siteBodyElement.querySelector(`.footer__statistics`);
 
-render(footerStatisticsElement, createFilmsCountTemplate());
-render(siteBodyElement, createFilmDetailsTemplate(films[0]));
+render(footerStatisticsElement, createFilmsCountTemplate(films.length));
+// render(siteBodyElement, createFilmDetailsTemplate(films[0]));
