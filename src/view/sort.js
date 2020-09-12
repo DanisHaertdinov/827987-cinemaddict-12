81f701ef433b92e5caf48fed1b7e6fda1508c7b2
Danis <danis.haertdinov@gmail.com>
2020-09-12 14:31:56 +0400

    показывает активную сортировку

--- src/view/sort.js
+++ src/view/sort.js
@@ -3,34 +3,29 @@ import {SortType} from "../const.js";
 
 const ACTIVE_BUTTON_CLASS = `sort__button--active`;
 
-const createSortTemplate = () => {
+const createSortTemplate = (currentSortType) => {
   return (
     `<ul class="sort">
-    <li><a href="#" class="sort__button ${ACTIVE_BUTTON_CLASS}" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
-    <li><a href="#" class="sort__button" data-sort-type="${SortType.DATE}">Sort by date</a></li>
-    <li><a href="#" class="sort__button" data-sort-type="${SortType.RATING}">Sort by rating</a></li>
+    <li><a href="#" class="sort__button ${currentSortType === SortType.DEFAULT ? ACTIVE_BUTTON_CLASS : ``}" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
+    <li><a href="#" class="sort__button ${currentSortType === SortType.DATE ? ACTIVE_BUTTON_CLASS : ``}" data-sort-type="${SortType.DATE}">Sort by date</a></li>
+    <li><a href="#" class="sort__button ${currentSortType === SortType.RATING ? ACTIVE_BUTTON_CLASS : ``}" data-sort-type="${SortType.RATING}">Sort by rating</a></li>
   </ul>`
   );
 };
 
 export default class Sort extends AbstractView {
-  constructor() {
+  constructor(sortType) {
     super();
 
     this._sortBtnClickHandler = this._sortBtnClickHandler.bind(this);
+    this._sortType = sortType;
   }
 
   _sortBtnClickHandler(evt) {
     if (evt.target.tagName === `A`) {
       evt.preventDefault();
       this._callback._sortBtnClick(evt.target.dataset.sortType);
-      this._setActiveBtn(evt.target);
     }
-  }
-
-  _setActiveBtn(btn) {
-    this.getElement().querySelector(`.${ACTIVE_BUTTON_CLASS}`).classList.remove(ACTIVE_BUTTON_CLASS);
-    btn.classList.add(ACTIVE_BUTTON_CLASS);
   }
 
   setSortBtnClickHandler(callback) {
@@ -39,6 +34,6 @@ export default class Sort extends AbstractView {
   }
 
   getTemplate() {
-    return createSortTemplate();
+    return createSortTemplate(this._sortType);
   }
 }
